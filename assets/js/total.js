0d8f5c7a2bae7bd6b1f11217bf59795485b94364
jonatas1n <jonatasgeek@gmail.com>
2024-08-06 14:34:06 -0300

    first commit

---
+++ assets/js/total.js
@@ -0,0 +1,47 @@
+const history = [];
+const CELL_VALUES = [2, 5, 10, 20, 50, 100];
+
+const totalEl = document.querySelector('.total span');
+
+function updateTotal() {
+  const cellValues = CELL_VALUES.map(value => {
+    const cell = document.getElementById(`cedula-${value}`);
+    return parseInt(cell.value) * value || 0;
+  });
+
+  const total = cellValues.reduce((acc, value) => acc + value, 0);
+  totalEl.innerText = total;
+}
+
+function updateCell(value, amount) {
+  if (!CELL_VALUES.includes(value)) {
+    return;
+  }
+  const cell = document.getElementById(`cedula-${value}`);
+  cell.value = parseInt(cell.value) + amount;
+}
+
+function decreaseCell(value) {
+  updateCell(value, -1);
+}
+
+function increaseCell(value) {
+  updateCell(value, 1);
+}
+
+function onUndo() {
+  const cellValue = history.pop();
+  decreaseCell(cellValue);
+  updateTotal();
+}
+
+function addMoney(amount) {
+  history.push(amount);
+  increaseCell(amount);
+  updateTotal();
+}
+
+CELL_VALUES.forEach(value => {
+  const cell = document.getElementById(`cedula-${value}`);
+  cell.addEventListener('input', updateTotal);
+});
